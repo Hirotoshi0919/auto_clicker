b652b7b1f355c5e6cf664fa7eecf799011e8cb66
Hirotoshi Kawakami <59823253+Hirotoshi0919@users.noreply.github.com>
2022-03-24 18:46:47 +0900

    コメント追加

--- auto_clicker.py
+++ auto_clicker.py
@@ -2,12 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
+# webdriverをブラウザのバージョンにあわせて更新
 driver = webdriver.Chrome(ChromeDriverManager().install())
+# ブラウザ最大化
 driver.maximize_window()
+# サイトを開く
 driver.get("https://orteil.dashnet.org/cookieclicker/")
-
+# 要素が読み込まれるまで最大10秒待機
 driver.implicitly_wait(10)
-
+# 無限クリック
 while True:
     cookie = driver.find_element(By.ID, "bigCookie")
     cookie.click()
